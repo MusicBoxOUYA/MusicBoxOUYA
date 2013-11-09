a846fe8903d7e51a13c2f7f5d7ac14a6f2a11eed
chuck-aka-ben <benjamin@thomasnetwork.net>
2013-11-09 16:51:03 -0500

    Added ajax functions

--- www/resources/js/bridge.js
+++ www/resources/js/bridge.js
@@ -8,3 +8,26 @@ function request(url, sendData, callBack){
 		}
 	})
 }
+
+function buildSongInfo(parentEle, res){
+	var data = JSON.parse(res);
+	var name = createElement("h3", {"class":"song-title"}, data.title);
+	var p = createElement("p");
+	var artist = createElement("span", {"class":"song-artist"}, data.artist);
+	var br = createElement("br");
+	var album = createElement("span", {"class":"song-album"}, data.album);
+	parentEle.html("");
+	insertElementAt(name, parentEle[0]);
+	insertElementAt(p, parentEle[0]);
+	insertElementAt(artist, p);
+	insertElementAt(br, p);
+	insertElementAt(album, p);
+}
+
+function buildTable(parentEle, res){
+	
+	var data = JSON.parse(res);
+	var table = createElement("table");
+	
+	insertElementAt(table, parentEle);
+}
