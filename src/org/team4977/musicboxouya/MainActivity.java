698aa6596a64002b25c6da0764ee7a0ac16487d9
Andrew <andrew@lobos.me>
2013-11-09 17:33:12 -0500

    Added LocalLibrary for loading music from a local filesystem Added libAztec for web serving Implemented libAztec in MainActivity

--- src/org/team4977/musicboxouya/MainActivity.java
+++ src/org/team4977/musicboxouya/MainActivity.java
@@ -1,5 +1,15 @@
 package org.team4977.musicboxouya;
 
+import java.io.IOException;
+
+import lobos.andrew.aztec.Aztec;
+import lobos.andrew.aztec.EasyRequestHandler;
+import lobos.andrew.aztec.HTTPRequest;
+import lobos.andrew.aztec.HTTPResponse;
+import lobos.andrew.aztec.RequestHandler;
+
+import org.team4977.musicboxouya.database.library.LocalLibrary;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
@@ -10,6 +20,29 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		final LocalLibrary library = new LocalLibrary("/sdcard/MusicTest");
+		
+		EasyRequestHandler reqHandler = new EasyRequestHandler();
+		reqHandler.registerPage("/api/library", new RequestHandler()
+		{
+
+			@Override
+			public HTTPResponse handleHTTPRequest(HTTPRequest req) {
+				return new HTTPResponse(200, library.toJSON());
+			}
+			
+		});
+		
+		try {
+			new Aztec(8080, reqHandler);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		library.refresh();
 	}
 
 	@Override
---
+++ src/org/team4977/musicboxouya/database/library/LocalLibrary.java
@@ -0,0 +1,67 @@
+package org.team4977.musicboxouya.database.library;
+
+import java.io.File;
+import java.io.FileInputStream;
+import org.team4977.musicboxouya.media.Album;
+import org.team4977.musicboxouya.media.Artist;
+import org.team4977.musicboxouya.media.Song;
+
+import android.media.MediaMetadataRetriever;
+
+public class LocalLibrary extends LibraryProvider {
+	
+	String path;
+	public LocalLibrary(String path)
+	{
+		this.path = path;
+	}
+	
+	protected void processDirectory(String dir)
+	{
+		System.out.println("Processing "+dir);
+		
+		File[] fileList = new File(dir).listFiles();
+		for ( int i = 0; i < fileList.length; i++ )
+		{
+			if ( fileList[i].isDirectory() )
+				processDirectory(fileList[i].getAbsolutePath());
+			else if ( fileList[i].isFile() && fileList[i].canRead() && fileList[i].getName().endsWith(".mp3") )
+			{
+				MediaMetadataRetriever metadata = new MediaMetadataRetriever();
+				System.out.println("Reading "+fileList[i].getAbsolutePath());
+				try {
+					metadata.setDataSource(new FileInputStream(fileList[i]).getFD());
+					String artistName = metadata.extractMetadata(MediaMetadataRetriever.METADATA_KEY_ARTIST);
+					if ( artistName == null )
+						artistName = "Unknown Artist";
+					
+					String albumName = metadata.extractMetadata(MediaMetadataRetriever.METADATA_KEY_ALBUM);
+					if ( albumName == null )
+						albumName = "Unknown Album";
+					
+					Artist artist = addArtist(artistName);
+					
+					Album album = artist.addAlbum(albumName);
+					
+					String title = metadata.extractMetadata(MediaMetadataRetriever.METADATA_KEY_TITLE);
+					int songID = Song.getNextID();
+					
+					Song s = new Song(songID, title, album, artist);
+					album.addSong(s);
+					addSong(s);
+					
+					metadata.release();
+				} catch (Exception e) {
+					System.out.println("Metadata get failed for "+fileList[i].getAbsolutePath());
+				}
+			}
+		}
+	}
+	
+	protected void doLibraryPopulate()
+	{
+		processDirectory(path);
+		System.out.println("DONE LIBRARY");
+	}
+
+}
