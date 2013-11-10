c831f552bfb9ec63cc13be7f396281c6f952ac49
Andrew <andrew@lobos.me>
2013-11-10 01:19:22 -0500

    Switched web server libaries

--- src/org/team4977/musicboxouya/MainActivity.java
+++ src/org/team4977/musicboxouya/MainActivity.java
@@ -2,13 +2,6 @@ package org.team4977.musicboxouya;
 
 import java.io.IOException;
 
-import lobos.andrew.aztec.Aztec;
-import lobos.andrew.aztec.BinarySafeHTTPResponse;
-import lobos.andrew.aztec.EasyRequestHandler;
-import lobos.andrew.aztec.HTTPRequest;
-import lobos.andrew.aztec.HTTPResponse;
-import lobos.andrew.aztec.RequestHandler;
-import lobos.andrew.aztec.SendableHTTPResponse;
 
 import org.team4977.musicboxouya.database.library.LibraryProvider;
 import org.team4977.musicboxouya.database.library.LibraryRefreshFinishedListener;
@@ -23,195 +16,16 @@ import android.view.Menu;
 public class MainActivity extends Activity implements LibraryRefreshFinishedListener {
 
 	MusicPlayer player = null; 
-	
+	WebServer webserver = null;
+	LocalLibrary library = null;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		final LocalLibrary library = new LocalLibrary("/sdcard/MusicTest");
+		library = new LocalLibrary("/sdcard/MusicTest");
 		library.setRefreshFinishedListener(this);
-		
-		EasyRequestHandler reqHandler = new EasyRequestHandler();
-		
-		reqHandler.registerPage("/api/library", new RequestHandler()
-		{
-
-			@Override
-			public HTTPResponse handleHTTPRequest(HTTPRequest req) {
-				if ( player == null )
-					return new HTTPResponse(200, "Waiting for library refresh...");
-				else
-					return new HTTPResponse(200, library.toJSON());
-			}
 			
-		});
-		
-		reqHandler.registerPage("/api/queue", new RequestHandler()
-		{
-			@Override
-			public HTTPResponse handleHTTPRequest(HTTPRequest req) {
-				if ( player == null )
-					return new HTTPResponse(200, "Waiting for library refresh...");
-				else
-				{
-					if ( !req.getParam("song").equals("") )
-					{
-						try
-						{
-							Song song = library.getSong(Integer.parseInt(req.getParam("song")));
-							if ( song != null )
-							{
-								player.queueSong(song);
-							}
-						} catch (Exception e) {
-							System.out.println("Request for song that does not exist!");
-						}
-					}
-					
-					if ( !req.getParam("limit").equals("") )
-					{
-						try {
-							int limit = Integer.parseInt(req.getParam("limit"));
-							return new HTTPResponse(200, player.getQueue().toJSON(limit));
-						}catch (Exception e) {}
-					}
-					return new HTTPResponse(200, player.getQueue().toJSON());
-				}
-					
-			} 
-		});
-		
-		reqHandler.registerPage("/api/nowplaying", new RequestHandler()
-		{
-			@Override
-			public HTTPResponse handleHTTPRequest(HTTPRequest req) {
-				if ( player == null )
-					return new HTTPResponse(200, "Waiting for library refresh...");
-				else
-					return new HTTPResponse(200, player.nowPlayingJSON());
-			}
-		});
-		
-		reqHandler.registerPage("/api/like", new RequestHandler()
-		{
-
-			@Override
-			public HTTPResponse handleHTTPRequest(HTTPRequest req) {
-				if ( player == null )
-					return new HTTPResponse(200, "Waiting for library refresh...");
-				else
-				{
-					try
-					{
-						Song song = library.getSong(Integer.parseInt(req.getParam("song")));
-						if ( song != null )
-						{
-							song.like();
-						}
-						else
-						{
-						return new HTTPResponse(200, "{\"success\": false}");
-						}
-					} catch (Exception e) {
-						return new HTTPResponse(200, "{\"success\": false}");
-					}
-					return new HTTPResponse(200, "{\"success\": true}");
-				}
-			}
-		});
-		
-		reqHandler.registerPage("/api/dislike", new RequestHandler()
-		{
-			@Override
-			public HTTPResponse handleHTTPRequest(HTTPRequest req) {
-				if ( player == null )
-					return new HTTPResponse(200, "Waiting for library refresh...");
-				else
-				{
-					try
-					{
-						Song song = library.getSong(Integer.parseInt(req.getParam("song")));
-						if ( song != null )
-						{
-							song.dislike();
-						}
-						else
-						{
-						return new HTTPResponse(200, "{\"success\": false}");
-						}
-					} catch (Exception e) {
-						return new HTTPResponse(200, "{\"success\": false}");
-					}
-					return new HTTPResponse(200, "{\"success\": true}");
-				}
-			}
-		});
-		
-		reqHandler.registerPage("/api/next", new RequestHandler()
-		{
-			@Override
-			public HTTPResponse handleHTTPRequest(HTTPRequest req) {
-				if ( player == null )
-					return new HTTPResponse(200, "Waiting for library refresh...");
-				else
-				{
-					player.next();
-					return new HTTPResponse(200, "{\"success\": true}");
-				}
-			}
-		});
-		
-		
-		reqHandler.registerPage("/api/pause", new RequestHandler()
-		{
-			@Override
-			public HTTPResponse handleHTTPRequest(HTTPRequest req) {
-				if ( player == null )
-					return new HTTPResponse(200, "Waiting for library refresh...");
-				else
-				{
-					player.pause();
-					return new HTTPResponse(200, "{\"success\": true, \"paused\": "+(player.isPaused()?"true":"false")+"}");
-				}
-			}
-		});
-		
-		reqHandler.registerPage("/api/art", new RequestHandler()
-		{
-			public SendableHTTPResponse handleHTTPRequest(HTTPRequest req) {
-				if ( player == null )
-					return new HTTPResponse(200, "Waiting for library refresh...");
-				else
-				{
-					Song song = library.getSong(Integer.parseInt(req.getParam("song")));
-					if ( song != null )
-					{
-						byte[] art = song.getRawArt();
-						if ( art != null )
-						{
-							return new BinarySafeHTTPResponse(200, HTTPResponse.stringForResponseCode(200), art, "image/bmp");
-						}
-						else
-						{
-							return new HTTPResponse(200, "No art");
-						}
-							
-					}
-					return new HTTPResponse(500, "Error");
-				}
-			}
-		});
-		
-		
-		try {
-			new Aztec(8080, reqHandler);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		
 		library.refresh();
 	}
 
@@ -226,6 +40,13 @@ public class MainActivity extends Activity implements LibraryRefreshFinishedList
 	public void libraryRefreshFinished(LibraryProvider library) {
 		System.out.println("Library load finished!");
 		player = MusicPlayer.getMusicPlayer();
+		webserver = new WebServer(library, player);
+		try {
+			webserver.start();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
---
+++ src/org/team4977/musicboxouya/WebServer.java
@@ -0,0 +1,179 @@
+package org.team4977.musicboxouya;
+
+import java.io.ByteArrayInputStream;
+import java.util.Map;
+
+import org.team4977.musicboxouya.database.library.LibraryProvider;
+import org.team4977.musicboxouya.media.Song;
+import org.team4977.musicboxouya.player.MusicPlayer;
+
+import fi.iki.elonen.NanoHTTPD;
+
+public class WebServer extends NanoHTTPD {
+	LibraryProvider library;
+	MusicPlayer player;
+	public WebServer(LibraryProvider library, MusicPlayer player)
+	{
+		super(8080);
+		this.library = library;
+		this.player = player;
+	}
+	
+    @Override
+    public Response serve(String uri, Method method, Map<String, String> header, Map<String, String> parms, Map<String, String> files) {
+    	// Static content
+    	if ( uri.equals("/") || uri.equals("/index.html") )
+    	{
+    		return new Response(Response.Status.OK, "text/html", WebServer.class.getResourceAsStream("/org/team4977/musicboxouya/www/index.html"));
+    	}
+    	else if ( uri.equals("/response.html") )
+    	{
+    		return new Response(Response.Status.OK, "text/html", WebServer.class.getResourceAsStream("/org/team4977/musicboxouya/www/response.html"));
+    	}
+    	else if ( uri.equals("/queue.html") )
+    	{
+    		return new Response(Response.Status.OK, "text/html", WebServer.class.getResourceAsStream("/org/team4977/musicboxouya/www/queue.html"));
+    	}
+    	else if ( uri.equals("/tvView.html") )
+    	{
+    		return new Response(Response.Status.OK, "text/html", WebServer.class.getResourceAsStream("/org/team4977/musicboxouya/www/tvView.html"));
+    	}
+    	
+    	// API
+    	else if ( uri.equals("/api/library") )
+    	{
+    		if ( player == null )
+				return new Response("Waiting for library refresh...");
+			else
+				return new Response(library.toJSON());
+    	}
+    	else if ( uri.equals("/api/queue") )
+    	{
+    		if ( player == null )
+				return new Response("Waiting for library refresh...");
+			else
+			{
+				if ( !(parms.get("song") == null) )
+				{
+					try
+					{
+						Song song = library.getSong(Integer.parseInt(parms.get("song")));
+						if ( song != null )
+						{
+							player.queueSong(song);
+						}
+					} catch (Exception e) {
+						System.out.println("Request for song that does not exist!");
+					}
+				}
+				
+				if ( parms.get("limit") != null )
+				{
+					try {
+						int limit = Integer.parseInt(parms.get("limit"));
+						return new Response(player.getQueue().toJSON(limit));
+					}catch (Exception e) {}
+				}
+				return new Response(player.getQueue().toJSON());
+			}
+    	}
+    	else if ( uri.equals("/api/nowplaying") )
+    	{
+    		if ( player == null )
+				return new Response("Waiting for library refresh...");
+			else
+				return new Response(player.nowPlayingJSON());
+    	}
+    	else if ( uri.equals("/api/like") )
+    	{
+    		if ( player == null )
+				return new Response("Waiting for library refresh...");
+			else
+			{
+				try
+				{
+					Song song = library.getSong(Integer.parseInt(parms.get("song")));
+					if ( song != null )
+					{
+						song.like();
+					}
+					else
+					{
+					return new Response("{\"success\": false}");
+					}
+				} catch (Exception e) {
+					return new Response("{\"success\": false}");
+				}
+				return new Response("{\"success\": true}");
+			}
+    	}
+    	else if ( uri.equals("/api/dislike") )
+    	{
+    		if ( player == null )
+				return new Response("Waiting for library refresh...");
+			else
+			{
+				try
+				{
+					Song song = library.getSong(Integer.parseInt(parms.get("song")));
+					if ( song != null )
+					{
+						song.dislike();
+					}
+					else
+					{
+					return new Response("{\"success\": false}");
+					}
+				} catch (Exception e) {
+					return new Response("{\"success\": false}");
+				}
+				return new Response("{\"success\": true}");
+			}
+		}
+    	else if ( uri.equals("/api/next") )
+    	{
+    		if ( player == null )
+				return new Response("Waiting for library refresh...");
+			else
+			{
+				player.next();
+				return new Response("{\"success\": true}");
+			}
+    	}
+    	else if ( uri.equals("/api/pause") )
+    	{
+    		if ( player == null )
+				return new Response("Waiting for library refresh...");
+			else
+			{
+				player.pause();
+				return new Response("{\"success\": true, \"paused\": "+(player.isPaused()?"true":"false")+"}");
+			}
+    	}
+    	else if ( uri.equals("/api/art") )
+    	{
+    		if ( player == null )
+				return new Response("Waiting for library refresh...");
+			else
+			{
+				Song song = library.getSong(Integer.parseInt(parms.get("song")));
+				if ( song != null )
+				{
+					byte[] art = song.getRawArt();
+					if ( art != null )
+					{
+						return new Response(Response.Status.OK, "image/bmp", new ByteArrayInputStream(art));
+					}
+					else
+					{
+						return new Response("No art");
+					}
+						
+				}
+				return new Response("Error");
+			}
+    	}
+    	
+		return new Response(Response.Status.NOT_FOUND, "text/html", "404");
+    }
+}
