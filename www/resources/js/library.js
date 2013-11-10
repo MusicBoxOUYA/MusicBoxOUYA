f58cf89a71e7169ef42a9662a99443d281a51f00
chuck-aka-ben <benjamin@thomasnetwork.net>
2013-11-09 23:44:16 -0500

    Added Song list view for library

--- www/resources/js/library.js
+++ www/resources/js/library.js
@@ -5,5 +5,21 @@ function call(){
   });
 }
 $( document ).ready(function(){
+  $("#view-album").click(function(){
+    $("dd").removeClass("active");
+    $(this).parent().addClass("active");
+    request("api/album.html", "", function(result){
+      buildAlbumList($("#album-list"), result);
+      $(document).foundation();
+    });
+  });
+  $("#view-song").click(function(){
+    $("dd").removeClass("active")
+    $(this).parent().addClass("active");
+    request("api/song-list.html", "", function(result){
+      buildSongTable($("#album-list"), JSON.parse(result));
+      $(document).foundation();
+    });
+  });
 });
 call();
